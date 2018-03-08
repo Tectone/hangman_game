0f36162edfb77b8830a7d0693f3288756a4105c7
Tectone <35806791+Tectone@users.noreply.github.com>
2018-03-08 10:06:12 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,93 @@
- //JavaScript Document
+(() => {
+  //put variables at top of js files everytime
+var words = ["banana", "frog", "cracker", "monkey", "chicken", "bucket", "steak", "cup"];
 
- console.log("Linked up");
+let currentWord = words[Math.floor(Math.random()*words.length)],
+    wordHint = document.querySelector('.currentWord'),
+    guessBox = document.querySelector('.selectedLetter'),
+    wrongGuesses = 0,
+    correctGuesses = 0,
+    resetScreen = document.querySelector('.reset_screen'),
+    resetButton = resetScreen.querySelector('button'),
+    wrongLetters = document.querySelector('.wrong_letters'),
+    wrongLetterArray = []; //2 square brackets means array.
 
+//functions go in the middle
+function showResetScreen(message) {
+  resetScreen.classList.add('show_piece');
+  resetScreen.querySelector('h3').textContent = message;
+}
 
+function resetGame() {
+  wrongGuesses = 0;
+  correctGuesses = 0;
+  wrongLetters.textContent = "";
+  guessBox.value = "";
+  wrongLetterArray = [];
+  let gamePieces = Array.from(document.querySelectorAll('.show_piece'));
+  gamePieces.forEach(piece => piece.classList.remove('show_piece'));
+  init();
+}
 
+function makeGuess() {
+  console.log(this.value);
+  if (this.value == "" || this.value.length < 1 ) {
+    return;
+  }
+  let currentGuess = this.value;
 
- let currentWord=null,
- 	wordHint = document.querySelector('.current-word'),
- 	guessBox = document.querySelector('.guess'),
- 	wrongGuesses = 0;
+  if (currentWord.indexOf(this.value) < 0) {
+  //this will check and keep track of wrong answers
+    if (wrongGuesses >= 6) {
+      console.log('you lose!');
+      showResetScreen("Game Over");
+    } else {
+      wrongLetterArray.push(this.value);// this will push the letter to the array
+      wrongLetters.textContent = wrongLetterArray.join(" ");
+      document.querySelector(`.wrong${wrongGuesses}`).classList.add('show_piece');
+    // increment the wrong with SVG
+      wrongGuesses++;
+    }
 
- 	function showResetScreen(){
- 		//user has lost, reset stuff and start over
- 		debugger;
- 		console.log('you lose, loser!');
- 	}
+} else { //This else matches the if on line 37
+  // person choose correct letter
+  // split the current word into an arrw so we can check the letter where the guess matches
+  var matchAgainst = currentWord.split('');
+  var hintString = wordHint.textContent.split(' '); // this splits characters not underscore
 
- 	function takeGuess() {
- 		console.log(this.value);
- 	}
+  // loop through the current word and check each letters
+  matchAgainst.forEach((letter, index) => {
+    if (letter === currentGuess) {
+      hintString[index] = currentGuess;
+      correctGuesses++
+    }
+    if (hintString.includes(currentGuess) === true) {
+      currentGuess--
+    }
 
- if (this.value =="" || this.value.length <1) {
- 	return.
- }
+  });
+  wordHint.textContent = "";
+  wordHint.textContent = hintString.join(" ");
 
- // set up the win and lose paths ( if / else )
- if (!currentWord.includes(this.value)) {
- 	console.log('invalid letter!');
- 	document.querySelector(`.wrong${wrongGuesses`).classList.add('show-piece');
+  if (correctGuesses === currentWord.length) {
+    showResetScreen("You won!");
+  }
 
- 	if (wrongGuesses <=5) {
- 		// increment the wrongGUesses variable
- 		wrongGuesses++;
- 	} else {
- 		// you lose, reset the game
- 		showResetScreen();
- 	}
+  }
+  guessBox.value = "";
+  console.log(currentWord);
+}
+resetButton.addEventListener('click', resetGame);
 
- 	// if (currentWord.includes(this.value)) {
- 	//console.log('valid letter!');    
- 	//above is what you type when you want it to match the correct letter and the one above this one is what happens when you want to match a wrong letter
- 	// losing path
- 	// compare the letter guess to the word to see if its in there
- 
- } else {
- 	// winning path
- }
+//this is the function to start the game
+function init() {
+  //picks a random number and matches it to a letter
+  currentWord = words[Math.floor(Math.random()*words.length)];
+  wordHint.textContent = currentWord.split("").map(letter => letter = "__").join(" ");
+}
+// the event handling always goes at the bottom
+guessBox.addEventListener('keyup', makeGuess);
+resetButton.addEventListener('click', resetGame);
+
+init();
+})();
